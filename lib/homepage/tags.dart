0f197b82917f151abbb8eea96dbcc4947229faf0
fredchen <fredchen1994@outlook.com>
2021-01-08 16:43:59 +0000

    rework tags section

--- lib/homepage/tags.dart
+++ lib/homepage/tags.dart
@@ -8,7 +8,7 @@ class Tags extends StatefulWidget {
 }
 
 class _TagsState extends State<Tags> {
-  final List<String> _tags = [
+  static const List<String> kTags = [
     'Food',
     'Tech',
     'Sport',
@@ -21,35 +21,56 @@ class _TagsState extends State<Tags> {
     'Drinking',
   ];
 
-  String _selected = '';
+  Map<String, bool> _tags;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO change to network
+    _tags = Map.fromIterable(
+      kTags,
+      key: (tag) => tag,
+      value: (tag) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     // https://stackoverflow.com/questions/56211844/flutter-web-mouse-hover-change-cursor-to-pointer
-    return Container(
-      child: RichText(
-        text: TextSpan(
-          children: _tags.map<TextSpan>(
-            (tag) {
-              return TextSpan(
-                text: '$tag ',
-                style: Theme.of(context).textTheme.bodyText1.copyWith(
-                      fontSize: 18.0,
-                      color: _selected == tag
-                          ? Theme.of(context).accentColor
-                          : Theme.of(context).backgroundColor,
-                    ),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    setState(() {
-                      _selected = tag;
-                    });
-                  },
-              );
-            },
-          ).toList(),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 8.0),
+        TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Enter search term',
+          ),
         ),
-      ),
+        const SizedBox(height: 8.0),
+        _suggestions(),
+      ],
+    );
+  }
+
+  Widget _suggestions() {
+    return Wrap(
+      spacing: 4.0,
+      runSpacing: 4.0,
+      children: _tags.entries.map<Widget>(
+        (entry) {
+          return ChoiceChip(
+            label: Text(entry.key),
+            selected: entry.value,
+            onSelected: (bool selected) {
+              setState(() {
+                _tags[entry.key] = selected;
+              });
+            },
+            selectedColor: Theme.of(context).accentColor,
+          );
+        },
+      ).toList(),
     );
   }
 }
