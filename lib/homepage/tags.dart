28c8016cad69575d8215eaaac5701950b7ef0669
fredchen <fredchen1994@outlook.com>
2021-02-05 18:50:23 +0000

    add workaround for hidden search box on mobile

--- lib/homepage/tags.dart
+++ lib/homepage/tags.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+import '../layout.dart';
 
 class Tags extends StatefulWidget {
   @override
@@ -49,6 +52,16 @@ class _TagsState extends State<Tags> {
         ),
         const SizedBox(height: 8.0),
         _suggestions(context),
+        // the following didn't work:
+        // KeyboardVisibilityBuilder(
+        //   builder: (context, isVisible) {
+        //     double padding = MediaQuery.of(context).viewInsets.bottom;
+        //     return SizedBox(height: isVisible ? padding : 0.0);
+        //   },
+        // ),
+        // workaround:
+        if (!isDisplayDesktop(context))
+          SizedBox(height: 256.0),
       ],
     );
   }
